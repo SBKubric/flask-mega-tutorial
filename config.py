adf62d4cc704157909ceb6bc98163f058ad83526
SBKubric <sbkubric@gmail.com>
2017-03-21 18:49:17 +0300

    Added login form

---
+++ config.py
@@ -0,0 +1,7 @@
+WTF_CRF_ENABLED = True
+SECRET_KEY = 'you-will-never-guess'
+OPENID_PROVIDERS = [
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
